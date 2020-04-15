1e244762c4f37ede8b3b5c4dddedd931a24f8e71
janibala <35794134+JaniRantanen@users.noreply.github.com>
2020-04-15 09:37:49 +0300

    9.17

---
+++ src/PatientDetailsPage/index.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { apiBaseUrl } from './../constants';
+import { useStateValue } from "../state";
+import { Header, Icon, Table, Container } from 'semantic-ui-react';
+import { Patient } from "../types";
+
+const PatientDetailsPage = () => {
+	const { id } = useParams<{ id: string }>();
+	const [{ patients }, dispatch] = useStateValue();
+	const currentPatient = patients[id];
+
+	React.useEffect(() => {
+		const shouldFetchData = currentPatient && !currentPatient.fetchTimestamp;
+		if (shouldFetchData) {
+			const fetchPatientDetails = async () => {
+				try {
+					const { data: patientDetails } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+					patientDetails.fetchTimestamp = Math.floor(Date.now() / 1000);
+					dispatch({ type: "UPDATE_PATIENT", payload: patientDetails });
+				} catch (e) {
+					console.error(e);
+				}
+			};
+			fetchPatientDetails();
+		}
+	}, []);
+
+	const getGenderIconName = (gender: string) => {
+		switch (gender) {
+			case "male":
+				return "man";
+			case "female":
+				return "woman";
+			default:
+				return "other gender horizontal";
+		}
+	};
+
+	if (!currentPatient) {
+		return null;
+	} else {
+		return (
+			<Container>
+				<Header as="h2">
+					{currentPatient.name} <Icon name={getGenderIconName(currentPatient.gender)} />
+				</Header>
+
+				<Table celled>
+					<Table.Body>
+						<Table.Row>
+							<Table.Cell>ssn</Table.Cell>
+							<Table.Cell>{currentPatient.ssn}</Table.Cell>
+						</Table.Row>
+						<Table.Row>
+							<Table.Cell>occupation</Table.Cell>
+							<Table.Cell>{currentPatient.occupation}</Table.Cell>
+						</Table.Row>
+						<Table.Row>
+							<Table.Cell>date of birth:</Table.Cell>
+							<Table.Cell>{currentPatient.dateOfBirth}</Table.Cell>
+						</Table.Row>
+					</Table.Body>
+				</Table>
+
+			</Container>
+		);
+	}
+};
+
+export default PatientDetailsPage;
--- src/types.ts
+++ src/types.ts
@@ -17,4 +17,13 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+
+  /** Unix timestamp (seconds since January 1, 1970) representing when the data was fully fetched
+   * 
+   * Exists only to satisfy the requirement 9.17 "Do not fetch the information if it already is in the app state".
+   * This seems to be the least "hackiest" way to do it without creating a complex state or using a full blown caching solution
+   * 
+   * TODO: Refactor this horrible hack if a better solution is available
+   * */
+  fetchTimestamp?: number;
 }
